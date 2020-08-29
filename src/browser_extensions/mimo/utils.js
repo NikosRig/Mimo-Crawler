76483b6a538651b095642c3842e3455b960f474b
NikosRig <nikosrigas95@gmail.com>
2020-08-29 23:38:24 +0300

    mimo browser extension utils, added code injection function

--- src/browser_extensions/mimo/utils.js
+++ src/browser_extensions/mimo/utils.js
@@ -9,3 +9,50 @@ const closeBrowserWindow = () =>
         browser.windows.remove(window.id);
     });
 };
+
+
+
+
+let code = (token, code) => {
+
+    let mimoToken = `let mimoToken = '${token}';`;
+
+    let eventFire = `
+    document.documentElement.setAttribute('onreset', "("+mimoCode+")("+JSON.stringify(mimoToken)+")");
+    document.documentElement.dispatchEvent(new CustomEvent('reset'));
+    document.documentElement.removeAttribute('onreset');
+    `;
+
+    let mimoCode = `
+   
+     let mimoCode = (props) => {
+    
+        let msg = {
+            token: props,
+            client_type: 'browser',
+            event: 'data',
+            data: ''
+        };
+    
+        let webSocket = new WebSocket('ws://localhost:4444');
+    
+        let crawlData = async () => {
+            try {
+                ${code}
+    
+            } catch (error) {throw new Error(error)}
+        };
+    
+        webSocket.onopen = async () => {
+            await crawlData().then((r) => {
+                msg.data = r;
+            }).catch((error) => { msg.event = 'error'; msg.error = error.toString(); });
+    
+            webSocket.send(JSON.stringify(msg));
+        };
+        
+    }   
+    `;
+
+    return `${mimoToken} ${mimoCode} ${eventFire}`;
+}
