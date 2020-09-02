e2bd7dc896a1696fd29b7f28952c5a0b02dcf120
NikosRig <nikosrigas95@gmail.com>
2020-09-02 21:25:22 +0300

    tab service methods return promises

--- src/browser_extensions/mimo/Services/tabService.js
+++ src/browser_extensions/mimo/Services/tabService.js
@@ -1,5 +1,5 @@
 
-class tabService  {
+class TabService  {
 
 
     createTab = url =>
@@ -10,52 +10,56 @@ class tabService  {
     }
 
 
-
-    /* --------------------- Close a tab  ---------------------
-    |  @param tabId {integer} @param delay {integer}
-    */
-
-    closeTab = (tabId, delay = 0) =>
-    {
-        setTimeout(() => {
-            browser.tabs.remove(tabId).catch( closeTabError =>
-            {
-                console.log(closeTabError) ;
-            });
-        }, delay);
-    }
-
-
     /*
-    * @param tabId {integer}
-    * @return {promise}
+    *  @param tabid {integer}
+    *  @param delay {integer}
+    *  @return {promise}
     * */
 
-    isTabExists = async (tabId) =>
-    {
-        return await browser.tabs.get(tabId).then(() => {
-            return true;
-        }).catch( error => { throw new Error(error); });
-    }
-
-    
-
-    /* --------------------------- Code injection  -----------------------------------
-    |  Inject js code in tab
-    |  @param tabId {integer} @param code {string} @param delay {integer}
-    |  @return {promise}
-    |
-    */
-
-    injectCode = (tabId, code, delay = 0) =>
+    closeTab = (tabid, delay = 0) =>
     {
         return new Promise((resolve, reject) => {
 
             setTimeout(() => {
 
-                this.isTabExists(tabId).catch( error => { reject(error); });
+                browser.tabs.remove(tabid).then(() => {
+                    resolve();
+                }).catch( closeTabError => { reject(closeTabError); });
 
-                browser.tabs.executeScript(tabId, { code: code }).then(() => {
+            }, delay);
+
+        });
+    }
+
+
+    /*
+    * @param tabid {integer}
+    * @return {promise}
+    * */
+
+    isTabExists = async (tabid) =>
+    {
+        return await browser.tabs.get(tabid).then(() => {
+            return true;
+        }).catch( error => { throw new Error(error); });
+    }
+
+
+
+    /*
+    *  @param tabId {integer}
+    *  @param code {string}
+    *  @param delay {integer}
+    *  @return {promise}
+    * */
+
+    injectCode = (tabid, code, delay = 0) =>
+    {
+        return new Promise((resolve, reject) => {
+
+            setTimeout( () => {
+
+                browser.tabs.executeScript(tabid, { code: code }).then(() => {
                     resolve();
                 }).catch( injectionError => { reject(injectionError); });
 
@@ -65,3 +69,6 @@ class tabService  {
     }
 
 }
+
+
+const tabService = new TabService();
