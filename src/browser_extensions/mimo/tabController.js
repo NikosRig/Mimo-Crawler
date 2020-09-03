378d53f05cb6c23c61ae9ac9daa33de439ad50dd
NikosRig <nikosrigas95@gmail.com>
2020-09-04 00:37:10 +0300

    tab controller

--- src/browser_extensions/mimo/tabController.js
+++ src/browser_extensions/mimo/tabController.js
@@ -1,18 +1,30 @@
-
-
 // NS_ERROR_NET_ON_WAITING_FOR    NS_ERROR_UNKNOWN_HOST (server not found)
-
-
 /*browser.webRequest.onErrorOccurred.addListener(
     (e) => {console.log(e)}, {urls: ["<all_urls>"]}
-
 )*/
 
 
-websocket.addEventListener('message', (message) =>
+
+const createAndInject = (message) =>
 {
-    let msg = JSON.parse(message.data);
+    tabService.createTab(message.url).then((tabinfo) => {
+
+        let code = codeInjectionBuilder(message.token, message.code);
+
+        return tabService.injectCode(tabinfo.id, code, message.injectionDelay);
+
+    }).catch(error => {
+        console.log(error)
+    });
+}
 
 
 
+websocket.addEventListener('message', (msg) =>
+{
+    let message = JSON.parse(msg.data);
+
+    createAndInject(message);
 });
+
+
