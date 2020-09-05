c4737a67e1aa353f4834a170ec5ae6d8c2ff485b
NikosRig <nikosrigas95@gmail.com>
2020-09-05 23:13:08 +0300

    message service initialization

---
+++ src/Services/MessageService.js
@@ -0,0 +1,58 @@
+
+
+class MessageService  {
+
+
+    constructor()
+    {
+        this.clients = [];
+    }
+
+
+    saveBrowser = (browserWebsocket) =>
+    {
+        this.browser = browserWebsocket;
+
+        this.browser.once('close', () => { delete this.browser; });
+
+        this.browser.on('error', (error) => { this.logger.error(error); });
+    }
+
+    saveClient = (websocketClient, token) =>
+    {
+        this.clients[token] = websocketClient;
+
+        websocketClient.on('error', (error) => { this.logger.error(error); });
+
+        websocketClient.on('close', () => { delete this.clients[token]; });
+    }
+
+    sendMessageToBrowser = (message) =>
+    {
+        if (!this.browser)
+            return;
+
+        this.browser.send(JSON.stringify(message));
+    }
+
+
+    sendMessageToClient = (message) =>
+    {
+        if (!this.isClientExists(message.token))
+            return;
+
+        this.clients[message.token].send(JSON.stringify(message));
+    }
+
+    isClientExists = (token) =>
+    {
+        return this.clients.hasOwnProperty(token);
+    }
+
+
+
+
+}
+
+
+module.exports = new MessageService();
