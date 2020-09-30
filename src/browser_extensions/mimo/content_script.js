ef06af2a4bd4567336a11641a80de89b913b4f62
NikosRig <nikosrigas95@gmail.com>
2020-10-01 00:13:25 +0300

    refactor: js injection

--- src/browser_extensions/mimo/content_script.js
+++ src/browser_extensions/mimo/content_script.js
@@ -1,25 +1,23 @@
-let mimoMainFunction = (token, custom_code) => {
+let mimoJsInjection = (token, custom_code) => {
 
     let websocket = new WebSocket('ws://localhost:4444/browser/crawl');
-
     let message = { token: token, errors: []};
 
     window.onerror =  (msg, url, lineNo, columnNo, error) => {
         message.errors.push(error.toString());
         websocket.send(JSON.stringify(message));
+        return true;
     }
 
-    let executeCustomCode = () => {
+    let executeCustomCode = async () => {
         return new Promise((response, reject) => { eval(custom_code); });
     };
 
     websocket.onopen = async () => {
 
-        await executeCustomCode().then((response) => {
+        await executeCustomCode().then(response => {
             message.responseData = response;
-        }).catch((error) => {
-            message.errors.push(error.toString())
-        });
+        }).catch(error => message.errors.push(error.toString()) );
 
         websocket.send(JSON.stringify(message)); //window.close();
     };
@@ -34,7 +32,7 @@ browser.runtime.sendMessage({ infoRequest: "tab_id" }).then((tab_id) => {
         let token = e[tab_id].token;
         let code = e[tab_id].code;
 
-        document.documentElement.setAttribute('onreset', "("+mimoMainFunction+")("+JSON.stringify(token)+','+JSON.stringify(code)+")");
+        document.documentElement.setAttribute('onreset', "("+mimoJsInjection+")("+JSON.stringify(token)+','+JSON.stringify(code)+")");
         document.documentElement.dispatchEvent(new CustomEvent('reset'));
         document.documentElement.removeAttribute('onreset');
 
