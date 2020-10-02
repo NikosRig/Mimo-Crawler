11c6083397e75d4fc903545f8d310c022d74c775
NikosRig <nikosrigas95@gmail.com>
2020-10-03 01:47:30 +0300

    refactor: controller.js

--- src/browser_extensions/mimo/controller.js
+++ src/browser_extensions/mimo/controller.js
@@ -1,35 +1,11 @@
 // NS_ERROR_NET_ON_WAITING_FOR    NS_ERROR_UNKNOWN_HOST (server not found)
 /*browser.webRequest.onErrorOccurred.addListener(
-    (e) => {console.log(e)}, {urls: ["<all_urls>"]}
+    (e) => {}, {urls: ["<all_urls>"]}
 )*/
 
 
 tabService.listenForTabIdRequests();
 
-tabService.listenForClosingTabs(
-    storage_service.removeTabIdFromStorage
-);
+tabService.onTabClose( storage_service.removeTabMessageFromStorage );
 
-websocket.addEventListener('message', (msg) =>
-{
-    let message = JSON.parse(msg.data);
-
-    tabService.createTab(message.url).then((tabinfo) => {
-
-        storage_service.saveTabIdInStorage(tabinfo.id, message);
-
-    }).catch(error => {
-        console.log(error)
-    });
-
-});
-
-
-
-
-
-
-
-
-
-
+websocket_service.onMessageEvent( createNewTabAndSaveTabMessage );
