2f9db29b8ccefe584ab37cd20c65126a795def09
NikosRig <nikosrigas95@gmail.com>
2020-10-10 23:22:36 +0300

    add: appConfig producationMode indicator

--- src/app/config/appConfig.js
+++ src/app/config/appConfig.js
@@ -1,6 +1,6 @@
 
 const appConfig = {
-
+    productionMode: true,
     websocketHost: '0.0.0.0',
     websocketPort: 4444,
     firefoxDeveloperEditionBinaryPath: '/usr/bin/firefox-developer-edition'
